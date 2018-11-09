d1b57efedc50b0952c9813225099c2b60b8d571c
Rabbit <yfhj1990@hotmail.com>
2018-11-09 14:28:11 +0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,22 +1,24 @@
 function createDraggable(el) {
-  let x = 0, ex = 0
+  let x = 0
+  
   el.addEventListener('mousedown', HandleMouseDown)
+  el.style.position = 'absolute'
+  el.style.left = el.getBoundingClientRect().x + 'px'
   
   function HandleMouseDown(evt) {
     x = evt.x
-    ex = el.getBoundingClientRect().x
     el.addEventListener('mousemove', HandleMouseMove)
     el.addEventListener('mouseup', HandleMouseUp)
   }
 
   function HandleMouseUp(evt) {
+    el.style.left = el.getBoundingClientRect().x + 'px'
+    el.style.transform = `translateX(0)`
     el.removeEventListener('mousemove', HandleMouseMove)
     el.removeEventListener('mouseup', HandleMouseUp)
   }
 
   function HandleMouseMove(evt) {
-    el.style.transform = `translateX(${ex + evt.x - x}px)`
+    el.style.transform = `translateX(${evt.x - x}px)`
   }
 }
-
-createDraggable(document.getElementById('el'))
